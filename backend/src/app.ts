28326a1c083e57b36a032482bbebb3c614950a21
Bhavesh Baraiya <113673807+itsbaraiya@users.noreply.github.com>
2025-12-15 02:19:22 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,7 @@
+// 
+// App
+// 
+
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
@@ -7,6 +11,7 @@ import rateLimit from "express-rate-limit";
 import userRoutes from "./routes/UserRoutes";
 import authRoutes from "./routes/AuthRoutes";
 import dashboardRoutes from "./routes/DashboardRoutes";
+import { authMiddleware } from "./middlewares/auth";
 import rfpRoutes from "./routes/RfpRoutes";
 
 dotenv.config();
@@ -18,48 +23,47 @@ const app = express();
  * ======================
  */
 app.use(helmet());
+
 app.use(
   rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000,
     max: 100,
   })
 );
 
 /**
  * ======================
- * BODY PARSERS
- * ======================
- */
-app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true, limit: "1mb" }));
-
-/**
- * ======================
- * CORS + UPLOADS
+ * CORS
  * ======================
  */
 const allowedOrigin = process.env.Frontend_URL;
+
 app.use(
-  "/api/uploads",
   cors({
     origin: allowedOrigin,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-  express.static(path.join(__dirname, "../uploads"))
+  })
 );
 
 /**
  * ======================
- * ROUTES
+ * BODY LIMITS
  * ======================
  */
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.use("/api/rfps", rfpRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-
 app.get("/", (req, res) => res.send("API is running"));
+app.use("/api/uploads", express.static(path.join(__dirname, "../uploads")));
+
 
 export default app;
+
